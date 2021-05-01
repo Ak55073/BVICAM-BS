dc0db7f166d30819c691f90e8fd4edd35e4205c6
Abhinav Kumar <ak55073@gmail.com>
2021-05-01 13:18:49 +0530

    Python Q3

--- Semester2/Python/mics_script/m1.py
+++
@@ -1,9 +0,0 @@
-def odd_even(number):
-    print("Even" if number % 2 == 0 else "Odd")
-
-
-try:
-    num = int(input("Enter a number: "))
-    odd_even(num)
-except ValueError:
-    print("Only Integer value are accepted.")
