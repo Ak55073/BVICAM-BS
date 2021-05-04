f60a85f342df1a9a1c3f6dd6af71d06b7ebb07c0
Abhinav Kumar <ak55073@gmail.com>
2021-05-04 13:14:03 +0530

    Python Q4

---
+++ Semester2/Python/Q4.py
@@ -0,0 +1,6 @@
+if __name__ == '__main__':
+    x = lambda num: num if (num % 7 == 0 and num % 5 != 0) else None
+    for i in range(1000, 2000):
+        val = x(i)
+        if val:
+            print(val)
